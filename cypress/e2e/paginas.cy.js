166fd636567923fb2ac255874a3ed32dc17622a3
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-04-19 17:47:37 -0300

    Teste login e direcionamento para página /home e página /cartões

---
+++ cypress/e2e/paginas.cy.js
@@ -0,0 +1,15 @@
+describe("Testando múltiplas páginas", () => {
+    it("Deve conseguir acessar a página de cartões", () => {
+        cy.visit("/")
+        cy.getByDataTest("botao-login").click();
+        cy.getByDataTest("email-input").type("lucas@gmail.com");
+        cy.getByDataTest("senha-input").type("123");
+        cy.getByDataTest("botao-enviar").click();
+
+        cy.location('pathname').should('eq', '/home');
+
+        cy.getByDataTest("app-home").find("a").eq(1).click();
+        cy.getByDataTest("titulo-cartoes").should('exist').and('have.text', 'Meus cartões');
+        cy.location('pathname').should("eq", '/home/cartoes')
+    })
+})
